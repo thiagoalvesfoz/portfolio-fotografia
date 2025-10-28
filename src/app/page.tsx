c25ea44d272b7672a1261d79ed0dde7c30215e94
Thiago Alves <thiago_marketingdigital@hotmail.com>
2025-10-27 23:25:53 -0300

    fix seo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,45 @@
-import { About } from "@/components/about";
-import { Contact } from "@/components/contact";
-import { Footer } from "@/components/Footer";
-import { Header } from "@/components/header";
-import { Hero } from "@/components/Hero";
-import { Portfolio } from "@/components/portfolio";
-import { WhatsAppButtton } from "@/components/WhatsAppButton";
-import { SEO } from "@/components/Head";
+import { About } from '@/components/about'
+import { Contact } from '@/components/contact'
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/header'
+import { Hero } from '@/components/Hero'
+import { Portfolio } from '@/components/portfolio'
+import { WhatsAppButtton } from '@/components/WhatsAppButton'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Julia de Lima - Fotografia e Portfólio',
+  description:
+    'Portfólio de Julia de Lima, fotógrafa especializada em retratos e fotografia artística.',
+  openGraph: {
+    title: 'Julia de Lima - Fotografia e Portfólio',
+    description:
+      'Portfólio de Julia de Lima, fotógrafa especializada em retratos e fotografia artística.',
+    url: 'https://juliadelima.vercel.app/',
+    siteName: 'Julia de Lima',
+    images: [
+      {
+        url: 'https://juliadelima.vercel.app/og.JPG',
+        width: 1200,
+        height: 630,
+        alt: 'Julia de Lima - Fotografia e Portfólio',
+      },
+    ],
+    locale: 'pt-BR',
+    type: 'website',
+  },
+}
 
 export default function Home() {
   return (
-      <>
-      <SEO />
-      <main className="min-h-screen relative">
-        <Header />
-        <Hero />
-        <About />
-        <Portfolio />
-        <Contact />
-        <Footer />
-        <WhatsAppButtton />
-      </main>
-      </>
-  
-  );
+    <main className="min-h-screen relative">
+      <Header />
+      <Hero />
+      <About />
+      <Portfolio />
+      <Contact />
+      <Footer />
+      <WhatsAppButtton />
+    </main>
+  )
 }
